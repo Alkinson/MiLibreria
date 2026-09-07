723e075a5fb11de847ae60c6cb6fcdad3cb15af4
Alkinson Gonzales <Alkinson.Gonzales@Akros.tech>
2026-09-07 15:34:44 -0600

    resta y multiplicacion

--- Class1.cs
+++ Class1.cs
@@ -4,4 +4,5 @@ public static class Calculadora
 {
     public static int Sumar(int a, int b) => a + b;
     public static int Restar(int a, int b) => a - b;
+    public static int Multiplicar(int a, int b) => a * b;
 }
